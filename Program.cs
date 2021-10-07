0fbdf39a7cc4d137e38f00ce28fb5f234388bd7c
Amdio Rocha <amdio0811@gmail.com>
2021-10-07 22:35:25 +0900

    Editando Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace cadastroDePecas
+namespace testeC_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o peso da peça:");
+            int peso = int.Parse(Console.ReadLine());
+            if (peso > 100)
+            {
+                Console.WriteLine("A peça possui o peso adequado para o cadastramento");
+            }else{
+                Console.WriteLine("O peso da peça é imcompativel para o cadastramento");
+            }
+            
+            string[] listaPecas = new string[3];
+            Console.WriteLine("Digite o nome das peças");
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < 3; i++){
+                listaPecas[i] = (s[i]);
+            }
+            Console.WriteLine("O nome das peças que vc cadastrou foi:");
+            for (int i = 0; i < 3; i++){
+                Console.WriteLine(s[i]);
+            }
+            
         }
     }
 }
