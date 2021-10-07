e4012c8d0fe9089771770a84a4e1fa9b23ad09cc
Amdio Rocha <amdio0811@gmail.com>
2021-10-07 22:30:19 +0900

    Criação do projeto Console dotnet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cadastroDePecas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
